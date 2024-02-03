32e28929598c71b8899471b9ed7b7a437cba1744
bd8036 <bd8036@naver.com>
2024-02-03 17:42:37 +0900

    init: init source

---
+++ src/main/kotlin/com/mrchoi/kotlinspringbootapi/KotlinSpringbootApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mrchoi.kotlinspringbootapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringbootApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mrchoi/kotlinspringbootapi/KotlinSpringbootApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mrchoi.kotlinspringbootapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
